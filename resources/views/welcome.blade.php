4d60488308e017c0aeb8db7f296160f481da7e25
Glen Bangkila <hadeinvane@gmail.com>
2019-05-24 10:34:54 +0800

    Update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Laravel</title>
+        <title>Bundy by Wenteractive</title>
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     </head>
     <body>
