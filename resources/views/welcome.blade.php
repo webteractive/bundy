555be87cb7afc733a8d1cd81315a8ef85df1a6df
Glen Bangkila <hadeinvane@gmail.com>
2019-05-24 10:39:18 +0800

    Fix spelling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Bundy by Wenteractive</title>
+        <title>Bundy by Webteractive</title>
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     </head>
     <body>
