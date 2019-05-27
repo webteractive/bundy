fa606fcf4baa8c51b00fb2fa1f21fc6202f71476
Glen  Bangkila <hadeinvane@gmail.com>
2019-05-28 03:59:02 +0800

    Adjust login form width

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="login bg-gray-100 border p-4 border-b-4 rounded">
-    <h1 class="mb-4 text-2xl">Sign In</h1>
+  <div class="flex min-h-screen justify-center items-center">
+    <div class="login bg-gray-100 border p-4 border-b-4 rounded">
+      <h1 class="mb-4 text-2xl">Sign In</h1>
 
-    <field
-      v-model="form.email"
-      label="Email"
-      type="text"
-      class="mb-4"
-    />
+      <field
+        v-model="form.email"
+        label="Email"
+        type="text"
+        class="mb-4"
+      />
 
-    <field
-      v-model="form.password"
-      label="Password"
-      type="password"
-      class="mb-4"
-    />
+      <field
+        v-model="form.password"
+        label="Password"
+        type="password"
+        class="mb-4"
+      />
 
-    <div class="field mb-6">
-      <label class="inline-flex items-center cursor-pointer">
-        <input type="checkbox" v-model="form.remember" class="mr-2" />
-        Remember me
-      </label>
+      <div class="field mb-6">
+        <label class="inline-flex items-center cursor-pointer">
+          <input type="checkbox" v-model="form.remember" class="mr-2" />
+          Remember me
+        </label>
+      </div>
+
+      <btn
+        label="Login"
+        class="bg-blue-500 text-white border-blue-600 border-b-2 rounded hover:bg-blue-600 hover:border-blue-700"
+        @click.native="login()"
+      />
     </div>
-
-    <btn
-      label="Login"
-      class="bg-blue-500 text-white border-blue-600 border-b-2 rounded hover:bg-blue-600 hover:border-blue-700"
-      @click.native="signIn()"
-    />
   </div>
 </template>
 
@@ -44,7 +46,7 @@ export default {
   },
 
   methods: {
-    signIn () {
+    login () {
       this.$http.post('/void/login', this.form)
         .then(({ data: { user } }) => {
           this.$store.dispatch('user/login', user)
@@ -53,3 +55,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.login {
+  width: 380px;
+}
+</style>
