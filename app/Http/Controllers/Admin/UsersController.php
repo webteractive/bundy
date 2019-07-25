d5c28454d1f316636471a58912859203a9d90195
Glen  Bangkila <hadeinvane@gmail.com>
2019-07-26 04:24:36 +0800

    Add unique email validation

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Bundy\UserManager;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use App\Bundy\Response\UserList;
 use App\Http\Controllers\Controller;
 
@@ -26,7 +27,7 @@ class UsersController extends Controller
             'role_id' => 'required',
             'last_name' => 'required',
             'first_name' => 'required',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users,email',
         ]));
     }
 
@@ -43,7 +44,11 @@ class UsersController extends Controller
             'role_id' => 'required',
             'last_name' => 'required',
             'first_name' => 'required',
-            'email' => 'required|email',
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('users')->ignore($id),
+            ],
         ]));
     }
 
