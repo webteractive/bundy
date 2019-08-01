42f8709dec12a5002b748bdc3b8f7a0aa4db90cb
Glen  Bangkila <hadeinvane@gmail.com>
2019-08-02 00:37:00 +0800

    Performance page UI/UX improvements

--- resources/js/components/PerformancePage.vue
+++ resources/js/components/PerformancePage.vue
@@ -30,7 +30,7 @@
       </div>
     </ct>
     <div class="relative">
-      <div class="border-b flex justify-center py-1">
+      <div class="border-b flex justify-between py-1">
         <ul class="text-sm font-normal">
           <li
             v-for="(className, label) in labels"
@@ -41,6 +41,13 @@
             <span v-text="label" class="capitalize ml-1" />
           </li>
         </ul>
+
+        <div class="pr-4">
+          <label>
+            <input type="checkbox" v-model="excludeWeekends">
+            <span>Exclude weekends</span>
+          </label>
+        </div>
       </div>
       <div class="flex">
         <div class="w-64 border-r">
@@ -66,13 +73,13 @@
         <div class="flex-1 overflow-y-auto max-w-full">
           <div class="flex flex-no-wrap">
             <div
-              v-for="day in daysInMonth"
+              v-for="(date, day) in datesInMonth"
               :key="`heading-day-${day}`"
               :class="{
-                'border-r': day !== daysInMonth,
-                'border-r-0': day === daysInMonth
+                'border-r': (day + 1) !== datesInMonth.length,
+                'border-r-0': (day + 1) === datesInMonth.length,
               }"
-              v-text="formatDate(dayToDate(day), 'ddd · MMM DD, YYYY')"
+              v-text="formatDate(date, 'ddd · MMM DD, YYYY')"
               class="w-48 px-0 py-2 flex-none border-b text-center"
             />
           </div>
@@ -82,16 +89,17 @@
             class="flex flex-no-wrap"
           >
             <div
-              v-for="day in daysInMonth"
+              v-for="(date, day) in datesInMonth"
               :key="`user-${user.id}-day-${day}`"
               :class="{
-                'border-r': day !== daysInMonth,
-                'border-r-0': day === daysInMonth,
-                [labels.late]: status(user, day).late(),
-                [labels.onTime]: status(user, day).onTime(),
-                [labels.future]: status(user, day).future(),
-                [labels.earlyBird]: status(user, day).earlyBird(),
-                [labels.absent]: !status(user, day).future() && status(user, day).absent(),
+                'border-r': (day + 1) !== datesInMonth.length,
+                'border-r-0': (day + 1) === datesInMonth.length,
+                [labels.late]: status(user, date).late(),
+                [labels.onTime]: status(user, date).onTime(),
+                [labels.future]: status(user, date).future(),
+                [labels.weekend]: status(user, date).weekend(),
+                [labels.earlyBird]: status(user, date).earlyBird(),
+                [labels.absent]: (!status(user, date).future() || !status(user, date).weekend()) && status(user, date).absent(),
               }"
               v-html="`&nbsp;`"
               class="h-20 w-48 flex-none border-b transition-bg-color"
@@ -124,7 +132,8 @@ export default {
 
   data () {
     return {
-      date: new Date()
+      date: new Date(),
+      excludeWeekends: false
     }
   },
 
@@ -138,6 +147,22 @@ export default {
       return getDaysInMonth(this.date)
     },
 
+    datesInMonth () {
+      let dates = []
+      const daysInMonth = getDaysInMonth(this.date)
+      for (let day = 1; day <= daysInMonth; day++) {
+        dates.push(setDate(this.date, day))
+      }
+
+      if (this.excludeWeekends) {
+        return dates.filter(date => {
+          return this.isWeekday(date)
+        })
+      }
+
+      return dates
+    },
+
     labels () {
       return {
         future: 'bg-gray-200',
@@ -145,6 +170,7 @@ export default {
         earlyBird: 'bg-green-500',
         late: 'bg-red-600',
         absent: 'bg-red-800',
+        weekend: 'bg-gray-200',
       }
     },
 
@@ -179,17 +205,23 @@ export default {
       return setDate(this.date, day)
     },
 
-    status ({ id }, day) {
+    isWeekend (date) {
+      return isSunday(date) || isSaturday(date)
+    },
+
+    isWeekday (date) {
+      return ! this.isWeekend(date)
+    },
+
+    status ({ id }, date) {
       const now = new Date()
-      const date = this.dayToDate(day)
       const inFuture = date => date.setHours(0,0,0,0) > new Date().setHours(0,0,0,0);
       const timeLog = this.timeLogs.find(timeLog => timeLog.user_id === id && isSameDay(timeLog.started_at, date))
       const future = inFuture(date)
       const absent = typeof timeLog === 'undefined'
       const late = ! absent && timeLog.late
       const onTime = ! absent && timeLog.on_time
-      const earlyBird = ! absent && timeLog.early_bird    
-      const isWeekend = isSunday(date) && isSaturday(date) 
+      const earlyBird = ! absent && timeLog.early_bird 
 
       return {
         late: () => late,
@@ -197,12 +229,12 @@ export default {
         future: () => future,
         onTime: () => onTime,
         earlyBird: () => earlyBird,
-        weekend: () => isWeekend
+        weekend: () => this.isWeekend(date)
       }
     },
 
     countLates ({ id }) {
-     return this.timeLogs.filter(timeLog => timeLog.user_id === id && timeLog.late).length
+      return this.timeLogs.filter(timeLog => timeLog.user_id === id && timeLog.late).length
     },
 
     back () {
