fdc5dec3df8d75b6b0fc3665fe7d7ff6c8e52e3c
Glen Gringgo Bangkila <hadeinvane@gmail.com>
2019-09-03 00:17:38 +0800

    Refactored login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,21 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Bundy\App;
-use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         if (auth()->check()) {
             return redirect('home');
         }
-        
+
         return new App('login');
     }
 }
