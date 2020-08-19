c7d8c163e3e1972a27424c94c7f2306d18beca5b
Glen Bangkila <hadeinvane@gmail.com>
2020-08-20 02:35:01 +0800

    Bump laravel version to 7

--- app/Timelog.php
+++ app/Timelog.php
@@ -4,7 +4,6 @@ namespace App;
 
 use App\Bundy\Streamable;
 use App\Bundy\ShouldStream;
-use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class TimeLog extends Model implements Streamable
